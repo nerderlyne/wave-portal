753c4476a9bd4daf01c6ef7a468d3e1979b418f5
nerderlyne <shivanshityagi@icloud.com>
2021-11-02 20:30:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, {useEffect, useState } from "react";
+import { ethers } from "ethers";
+import waveportal from './utils/WavePortal.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+  return (<>
+    <h1>Hello world!</h1>
+    <p>Please work.</p>
+  </>)
 }
 
 export default App;
+
